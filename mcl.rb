f191f549426707c28d8de09df1f603ac11e09796
masuldev <kimmoimm@gmail.com>
2024-09-02 10:29:11 +0900

    Brew formula update for mcl version v1.0.1

--- mcl.rb
+++ mcl.rb
@@ -5,20 +5,20 @@
 class Mcl < Formula
   desc "mcl is interactive CLI"
   homepage ""
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     on_intel do
-      url "https://github.com/masuldev/mcl/releases/download/v1.0.0/mcl_1.0.0_darwin_amd64.tar.gz"
-      sha256 "312e95aa4d48e24bf812220e11381abc7e00d669727455eae378e7ac1554f6f6"
+      url "https://github.com/masuldev/mcl/releases/download/v1.0.1/mcl_1.0.1_darwin_amd64.tar.gz"
+      sha256 "463e2494777522a16f2c3e4be7b5ce6dfcf7c2b701f8c969ca6588dc8e64dbe4"
 
       def install
         bin.install "mcl"
       end
     end
     on_arm do
-      url "https://github.com/masuldev/mcl/releases/download/v1.0.0/mcl_1.0.0_darwin_arm64.tar.gz"
-      sha256 "02023cc99da4ffa334605506fec07a840078283c7d6aa45d799b293c1b5d5207"
+      url "https://github.com/masuldev/mcl/releases/download/v1.0.1/mcl_1.0.1_darwin_arm64.tar.gz"
+      sha256 "e17fb959e26d72c19a4a68da58f4b55f7397cc36a058725883f4527401cb254d"
 
       def install
         bin.install "mcl"
@@ -29,8 +29,8 @@ class Mcl < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/masuldev/mcl/releases/download/v1.0.0/mcl_1.0.0_linux_amd64.tar.gz"
-        sha256 "f9825fcc22d7114461c2ce7598928b67222431c5a3b869d4fae4bcb745b3eef5"
+        url "https://github.com/masuldev/mcl/releases/download/v1.0.1/mcl_1.0.1_linux_amd64.tar.gz"
+        sha256 "eeee701ff6b8e68c6f3d4ef453826075d477fb274bdaa1b5478b4591c7d56016"
 
         def install
           bin.install "mcl"
@@ -39,8 +39,8 @@ class Mcl < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/masuldev/mcl/releases/download/v1.0.0/mcl_1.0.0_linux_arm64.tar.gz"
-        sha256 "246cc1c2f2be2d4b5141867f1dae90387445c14b5be0cfbbdd5c4e7fc9740636"
+        url "https://github.com/masuldev/mcl/releases/download/v1.0.1/mcl_1.0.1_linux_arm64.tar.gz"
+        sha256 "160d0c7a27ee617647f51a0a387e858729ae3884a33700941da1ab1b20fd8a8e"
 
         def install
           bin.install "mcl"

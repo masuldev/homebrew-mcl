7fb318c44ff115e33d155938506ec24b09a7bd2d
masuldev <masuldev@gmail.com>
2025-07-22 07:30:15 +0000

    Update mcl formula for v1.2.0

--- mcl.rb
+++ mcl.rb
@@ -1,9 +1,9 @@
 class Mcl < Formula
   desc "My CLI for AWS"
   homepage "https://github.com/masuldev/mcl"
-  url "https://github.com/masuldev/mcl/releases/download/v1.1.6/mcl"
+  url "https://github.com/masuldev/mcl/releases/download/v1.2.0/mcl"
   sha256 ""
-  version "1.1.6"
+  version "1.2.0"
 
   def install
     bin.install "mcl"
